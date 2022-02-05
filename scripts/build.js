4a770037f730edc5a9164a1e24780186797f3827
Devesh Kumar <devesh2027@gmail.com>
2022-02-05 15:03:57 +0530

    Setup script for basic one-level MD builds to HTML

---
+++ scripts/build.js
@@ -0,0 +1,52 @@
+function buildPages() {
+	const readPagesDirectory = require("../helpers/readPagesDirectory");
+
+	const markdownFiles = readPagesDirectory();
+
+	const dirExists = require("../helpers/dirExists");
+	const fs = require("fs");
+	const path = require("path");
+
+	const buildFolder = path.resolve(process.cwd(), "./build");
+	const publicFolder = path.resolve(process.cwd(), "./public");
+
+	if (dirExists(buildFolder)) fs.rmSync(buildFolder, { recursive: true });
+	fs.mkdirSync(buildFolder);
+
+	if (markdownFiles.length) {
+		const {
+			marked: { parse: parseMarkdown },
+		} = require("marked");
+
+		for (let fileName of markdownFiles) {
+			console.log("Building page: ", fileName);
+			const markdownContent = fs.readFileSync(fileName, "utf-8");
+			const convertedHTML = parseMarkdown(markdownContent);
+
+			fs.writeFileSync(
+				`${buildFolder}/${
+					fileName
+						.split(path.resolve(process.cwd(), "./pages"))[1] // Get the full file name for the markdown file
+						.split(".md")[0] // Remove the .md extension
+				}.html`,
+				convertedHTML
+			);
+		}
+		console.log("Finished Building Pages");
+		console.log("Moving static assets to build directory");
+		if (dirExists(publicFolder)) {
+			// For all static assets
+			const staticAssets = fs.readdirSync(publicFolder);
+			for (let assetFile of staticAssets)
+				fs.copyFileSync(
+					`${publicFolder}/${assetFile}`,
+					`${buildFolder}/${assetFile}`
+				);
+		}
+	}
+
+	console.log("Build successful");
+	return process.exit(0); // Done building without any issues
+}
+
+module.exports = buildPages;
--- scripts/index.js
+++ scripts/index.js
@@ -11,6 +11,8 @@ if (commandType === "dev") {
 	// Start the dev server, with live reloading for changes in the 'pages' directory.
 } else if (commandType === "build") {
 	// Build the pages directory and generate a 'build' folder.
+	const build = require("./build");
+	build();
 } else if (commandType === "start") {
 	// Check if there is a build folder, if yes, use the 'serve' package to serve it's content on a local server.
 }
