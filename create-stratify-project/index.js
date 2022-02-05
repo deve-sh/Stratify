e57b3d336703063e536b13ee7c72f6d6f5acb23c
Devesh Kumar <devesh2027@gmail.com>
2022-02-05 13:48:13 +0530

    Added create-stratify-project boilerplate generator script

---
+++ create-stratify-project/index.js
@@ -0,0 +1,94 @@
+#!/usr/bin/env node
+
+const { execSync } = require("child_process");
+const fs = require("fs");
+
+function generateError(errorMessage) {
+	console.error(errorMessage);
+	return process.exit(1);
+}
+
+const packageJSONContent = (appName) => `{
+	"name": "${appName}",
+	"version": "1.0.0",
+	"description": "A simple tool to build out a static website using nothing but Markdown.",
+	"main": "index.js",
+	"scripts": {
+	  "test": "echo \\"Error: no test specified\\" && exit 1",
+	  "dev": "stratify dev",
+	  "build": "stratify build",
+	  "start": "stratify start"
+	},
+	"dependencies": {
+		"stratify-web": "^1.0.0"
+	},
+	"keywords": [],
+	"author": "",
+	"license": "UNLICENSED"
+}`;
+
+const filesToGenerate = [
+	{
+		name: ".gitignore",
+		content: "node_modules\n.env\nbuild\n",
+	},
+	{
+		name: "package.json",
+		content: ({ appName }) => packageJSONContent(appName),
+	},
+	{
+		name: "pages/index.md",
+		requiresFolder: "pages",
+		content: `# This is a Sample Page
+
+Populate this with content, run \`npm run dev\` to see this page change live.
+		
+Once you're done with working on the files, run \`npm run build\` to compile the entire directory to static HTML Files.
+		
+Run \`npm run start\` to serve the built output.`,
+	},
+];
+
+function generateStratifyProject() {
+	let directoryName = process.argv[2];
+	const appName = process.argv[3] || "stratify-boilerplate";
+
+	if (!directoryName)
+		return generateError(
+			`Error: Please provide a directory name as the first command line argument.\n` +
+				`Example: npx create-stratify-project directory-name\n`
+		);
+
+	directoryName = directoryName.replace("./", "");
+
+	// Create directory for project
+	const directory = `./${directoryName}`;
+
+	if (fs.existsSync(directory))
+		return generateError(
+			"A directory with the specified name already exists. Please remove it first."
+		);
+
+	fs.mkdirSync(directory);
+	// Write pre-requisite files to the directory.
+	for (let file of filesToGenerate) {
+		if (file.requiresFolder)
+			fs.mkdirSync(`${directory}/${file.requiresFolder}`);
+		fs.writeFileSync(
+			`${directory}/${file.name}`,
+			typeof file.content === "function"
+				? file.content({ appName, directory })
+				: file.content
+		);
+	}
+	execSync(`cd ${directory}`);
+	// Install dependencies
+	execSync("npm install");
+	// Initialize it as a git repositry
+	execSync("git init");
+	// Come back to the main folder
+	execSync("cd ../");
+	console.log("Successfully generated startify project.");
+}
+
+generateStratifyProject();
