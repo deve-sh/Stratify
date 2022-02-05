0ec5ce40ceda8eb575cdbcc1fc7ed1842e7b9299
Devesh Kumar <devesh2027@gmail.com>
2022-02-05 14:06:21 +0530

    Setup base for 'stratify' command

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const commandType = process.argv[2];
+
+if (!commandType) return "";
+
+if (commandType === "dev") {
+	// Start the dev server, with live reloading for changes in the 'pages' directory.
+} else if (commandType === "build") {
+	// Build the pages directory and generate a 'build' folder.
+} else if (commandType === "start") {
+	// Check if there is a build folder, if yes, use the 'serve' package to serve it's content on a local server.
+}
