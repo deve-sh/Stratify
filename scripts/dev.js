c336a0bd59bea6d3faa849e1641f84ed9a4d0143
Devesh Kumar <devesh2027@gmail.com>
2022-02-06 10:12:07 +0530

    Completed dev script with live-server

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,30 +1,70 @@
+const buildPage = require("../helpers/buildPage");
+
 function setupDevServer() {
 	console.log("Starting up dev server");
 	const liveServer = require("live-server");
 
 	// Setup initial build in .live folder
 	const buildPages = require("./build");
-	buildPages("./.live", true);
+	buildPages("./.live", true, false);
 
 	// Initial build done.
 	// Setup a change listener for listening to user's changes in the pages directory.
 	const fs = require("fs");
 	const path = require("path");
-	fs.watch(
-		path.resolve(process.cwd(), "./pages"),
-		{ recursive: true },
-		console.log
-	);
+	const buildFolder = path.resolve(process.cwd(), "./.live");
+
+	// For debouncing
+	let lastFileChanged = null;
+	let lastFileChangeTime = null;
+
+	try {
+		fs.watch(
+			path.resolve(process.cwd(), "./pages"),
+			{ recursive: true }, // Listen to all internal pages
+			(type, fileName) => {
+				// Only rebuild this page
+				if (type === "change") {
+					// Simple debouncing to check if there aren't duplicate file changes.
+					if (
+						lastFileChanged === fileName &&
+						lastFileChangeTime &&
+						new Date().getTime() - lastFileChangeTime.getTime() < 150
+					)
+						return;
+					lastFileChangeTime = new Date();
+					lastFileChanged = fileName;
+
+					// Only build the page that has very recently been changed
+					return buildPage(
+						{
+							fileName: path.resolve(process.cwd(), "./pages", `./${fileName}`),
+							directory: "",
+						},
+						buildFolder
+					);
+				}
+				console.warn(
+					"Renames or deletions for pages require a dev server restart."
+				);
+				process.exit(1);
+			}
+		);
+	} catch (_) {
+		// On linux, recursive watch is not supported. Hence this causes script crashes.
+	}
 
 	// Change listener setup, start the live server for changes in the .live directory.
+	console.log(
+		"Starting up Live Server, just make changes to your pages and reload your browser window if you don't immediately see them"
+	);
 	const liveServerParams = {
 		port: 8181,
 		host: "0.0.0.0",
-		root: path.resolve(process.cwd(), "/.live"),
+		root: path.resolve(process.cwd(), "./.live"),
 		open: true,
-		file: "index.html",
 		wait: 1000,
-		logLevel: 2,
+		logLevel: 1,
 	};
 	liveServer.start(liveServerParams);
 }
