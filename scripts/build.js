10885b7ce61f6b6290962b9dfa040e30d42fa859
Devesh Kumar <devesh2027@gmail.com>
2022-02-06 09:35:11 +0530

    Added flexibility of build path to build script

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,4 @@
-const getPageTemplate = require("../helpers/getPageTemplate");
-
-function buildPages() {
+function buildPages(buildPath = "./build") {
 	const readPagesDirectory = require("../helpers/readPagesDirectory");
 
 	const markdownFiles = readPagesDirectory();
@@ -9,41 +7,16 @@ function buildPages() {
 	const fs = require("fs");
 	const path = require("path");
 
-	const buildFolder = path.resolve(process.cwd(), "./build");
+	const buildFolder = path.resolve(process.cwd(), buildPath);
 	const publicFolder = path.resolve(process.cwd(), "./public");
 
 	if (dirExists(buildFolder)) fs.rmSync(buildFolder, { recursive: true });
 	fs.mkdirSync(buildFolder);
 
 	if (markdownFiles.length) {
-		const parseMarkdown = require("../helpers/parseMarkdown");
+		const buildPage = require("../helpers/buildPage");
+		for (let file of markdownFiles) buildPage(file, buildFolder);
 
-		for (let { fileName, directory } of markdownFiles) {
-			const pageName = fileName
-				.split(path.resolve(process.cwd(), "./pages"))[1] // Get the full file name for the markdown file
-				.split(".md")[0] // Remove the .md extension;
-				.replace("\\", "/"); // Remove opposite slashes from the path
-
-			console.log("Building page: ", pageName);
-			const markdownContent = fs.readFileSync(fileName, "utf-8");
-			const { html: convertedHTML, title = "" } =
-				parseMarkdown(markdownContent);
-
-			if (directory)
-				fs.mkdirSync(`${buildFolder}${directory}`, { recursive: true });
-
-			// Check if there is any template present for this page.
-			const template = getPageTemplate(pageName);
-			if (template)
-				fs.writeFileSync(
-					`${buildFolder}/${pageName}.html`,
-					// Replace the  {{ content }} block with the converted markdown HTML
-					template
-						.replace("{{ content }}", convertedHTML)
-						.replace("{{ title }}", title)
-				);
-			else fs.writeFileSync(`${buildFolder}/${pageName}.html`, convertedHTML);
-		}
 		console.log("Finished Building Pages");
 		console.log("Moving static assets to build directory");
 		if (dirExists(publicFolder)) {
