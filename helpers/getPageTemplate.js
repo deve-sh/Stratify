1885979bd4de78e4ad33dbe9867fdb26e36d2443
Devesh Kumar <devesh2027@gmail.com>
2022-05-12 18:09:20 +0530

    1.3.0: Added process.env.STRATIFY_APP parsing and replacement in page template

--- helpers/getPageTemplate.js
+++ helpers/getPageTemplate.js
@@ -1,4 +1,4 @@
-const getPageTemplate = (pageName) => {
+const getPageTemplate = (pageName, environmentVariables = {}) => {
 	const fs = require("fs");
 	const path = require("path");
 
@@ -11,10 +11,29 @@ const getPageTemplate = (pageName) => {
 		`./templates/index.html`
 	);
 
+	let pageTemplateContent = null;
 	if (fs.existsSync(pageProbableTemplatePath))
-		return fs.readFileSync(pageProbableTemplatePath, "utf8");
+		pageTemplateContent = fs.readFileSync(pageProbableTemplatePath, "utf8");
 	if (fs.existsSync(defaultTemplatePath))
-		return fs.readFileSync(defaultTemplatePath, "utf8");
+		pageTemplateContent = fs.readFileSync(defaultTemplatePath, "utf8");
+	if (pageTemplateContent) {
+		// Check for environment variables and replace occurrences of 'process.env.STRATIFY_APP_' with the original environment variable values.
+		for (let variableName of Object.keys(environmentVariables || {})) {
+			if (variableName.startsWith("STRATIFY_APP_")) {
+				// \b for whole-word searches as process.env.STRATIFY_APP_ENV and
+				// process.env.STRATIFY_APP_ENV2 will be replaced by value of process.env.STRATIFY_APP_ENV without it.
+				const variableNameRegex = new RegExp(
+					`\\bprocess.env.${variableName}\\b`,
+					"g"
+				);
+				pageTemplateContent = pageTemplateContent.replace(
+					variableNameRegex,
+					environmentVariables[variableName]
+				);
+			}
+		}
+		return pageTemplateContent;
+	}
 	return null;
 };
 
